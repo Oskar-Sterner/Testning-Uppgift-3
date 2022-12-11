e2a5439e950aa8a7547e61e693c49152f53535ac
Oskar Sterner <oskar.lr.sterner@gmail.com>
2022-12-11 22:04:45 +0100

    fix

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -109,45 +109,6 @@ describe("should test out movieApp", () => {
 
   it("should return interceptData", () => {
     let searchText: string = "Lord";
-    const interceptMovies: IOmdbResponse = {
-      Search: [
-        {
-          Title: "The Lord of the Rings: The Fellowship of the Ring",
-          Year: "2001",
-          imdbID: "tt0120737",
-          Type: "movie",
-          Poster: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
-        },
-        {
-          Title: "The Lord of the Rings: The Return of the King",
-          Year: "2003",
-          imdbID: "tt0167260",
-          Type: "movie",
-          Poster: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-          Title: "The Lord of the Rings: The Two Towers",
-          Year: "2002",
-          imdbID: "tt0167261",
-          Type: "movie",
-          Poster: "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-        },
-        {
-          Title: "Lord of War",
-          Year: "2005",
-          imdbID: "tt0399295",
-          Type: "movie",
-          Poster: "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
-        },
-        {
-          Title: "The Lord of the Rings: The Rings of Power",
-          Year: "2022–",
-          imdbID: "tt7631058",
-          Type: "series",
-          Poster: "https://m.media-amazon.com/images/M/MV5BNTg3NjcxYzgtYjljNC00Y2I2LWE3YmMtOTliZTkwYTE1MmZiXkEyXkFqcGdeQXVyNTY4NDc5MDE@._V1_SX300.jpg"
-        },
-      ]
-    };
 
     cy.intercept("GET", "http://omdbapi.com/*", { interceptMovies }).as(
       "interceptMovies"
